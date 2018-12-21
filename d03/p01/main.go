aefee5d6fb4c8e19f77f2618129b7ee9821a346b
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-12-21 07:14:55 +0100

    Add parsing for input

--- d03/p01/main.go
+++ d03/p01/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /**
 	The Elves managed to locate the chimney-squeeze prototype fabric for Santa's suit
 	(thanks to someone who helpfully wrote its box IDs on the wall of the warehouse in the middle of the night).
@@ -61,10 +69,63 @@ type claim struct {
 
 // #1 @ 1,3: 4x4
 
-func newClaim(line string) claim {
+func newClaim(line string) (claim, error) {
+	parts := strings.Split(line, "@")
+	boxID, err := strconv.Atoi(strings.TrimSpace(parts[0])[1:])
+	if err != nil {
+		return claim{}, err
+	}
 
+	subParts := strings.Split(parts[1], ":")
+	offsets := strings.Split(strings.TrimSpace(subParts[0]), ",")
+	left, err := strconv.Atoi(offsets[0])
+	if err != nil {
+		return claim{}, err
+	}
+	top, err := strconv.Atoi(offsets[1])
+	if err != nil {
+		return claim{}, err
+	}
+
+	area := strings.Split(strings.TrimSpace(subParts[1]), "x")
+	wide, err := strconv.Atoi(area[0])
+	if err != nil {
+		return claim{}, err
+	}
+	tall, err := strconv.Atoi(area[1])
+	if err != nil {
+		return claim{}, err
+	}
+
+	return claim{
+		id:   boxID,
+		left: left,
+		top:  top,
+		wide: wide,
+		tall: tall,
+	}, nil
 }
 
 func main() {
+	f, err := os.Open("../data/input.txt")
+	if err != nil {
+		log.Fatalf("could not read from file: %v\n", err)
+	}
+	defer f.Close()
 
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		claim, err := newClaim(line)
+		if err != nil {
+			log.Fatalf("could not convert line into claim: %v\n", err)
+		}
+
+		log.Println(claim)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("error while scanning: %v\n", err)
+	}
 }
